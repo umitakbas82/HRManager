1cf161549a35e60e426365b93113a0962e1638a8
umitakbas82 <umit.akbas@hotmail.com.tr>
2022-01-29 00:28:06 +0300

    Add project files.

---
+++ Entities/Concrete/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Employee
+    {
+        public string EmployeeName { get; set; }
+        public string EmployeeSurname { get; set; }
+        public DateTime EmployeeBirthDate { get; set; }
+        public string EmlployeeGraduate { get; set; }
+        public string EmployeeRole { get; set; }
+        public string EmployeeAdrress { get; set; }
+        public string EmployeePhoneNumber { get; set; }
+        public string EmployeeCellPhoneNumber { get; set; }
+        public int EmployeeIBANNumber { get; set; }
+        public string EmployeeEMailAdress { get; set; }
+        public decimal EmployeeSalary{ get; set; }
+    }
+}
